57ed6e55a098e34b3c89825c5d3982c218e896ca
Shihab - On the Code <144271290+shihabonthecode@users.noreply.github.com>
2023-09-09 01:05:21 +0600

    Update app.js

--- Project-1-color_flipper/app.js
+++ Project-1-color_flipper/app.js
@@ -1,15 +1,1 @@
-const hexCode = document.querySelector('#hexCode');
-const hexBtn = document.querySelector('#hexBtn');
-const body = document.querySelector('body')
-const color = ['A', 'B', 'C', 'D', 'E', "F", 1, 2, 4, 5, 6, 7, 8, 9, 0];
-
-hexBtn.addEventListener('click', () => {
-    let genCode = ''; 
-    for (let i = 0; i < 6; i++) {
-        let colorRange = Math.floor(Math.random() * color.length);
-        genCode += color[colorRange];
-    }
-    hexCode.innerHTML = `#${genCode}`;
-    body.style.background = `#${genCode}`;
-});
 
